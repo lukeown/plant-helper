940ae2eba6ce718a3fdc3b7a8e46135fc688f7d2
lukeown <lukeown@gmail.com>
2020-10-09 12:16:45 -0400

    Add ability to write to Firebase database

--- script.js
+++ script.js
@@ -1,53 +1,23 @@
-//function submitEvent(event) {
-    //event.preventDefault()
+var config = {
+  apiKey: "AIzaSyBfUzdoYxk26tQj7MuQt9CU4Qj7TflOahA",
+  authDomain: "planter-8ffdc.firebaseapp.com",
+  databaseURL: "https://planter-8ffdc.firebaseio.com",
+  storageBucket: "planter-8ffdc.appspot.com",
+};
 
-    //const name = document.getElementById('name').value
-    //const phone = document.getElementsById('phone').value
-    //output.innerText = `Name = ${name} Phone = ${phone}`
-//}
+firebase.initializeApp(config);
 
-//const output = document.getElementById('output')
-//const form = document.getElementById('myForm')
-//form.addEventListener('submit', submitEvent)
+var myFirebase = firebase.database().ref();
+var recommendations = myFirebase.child("recommendations");
+var commonName = []
 
-//function submitEvent(event) {
-  //  event.preventDefault()
+function addPlant() {
+  var commonName = document.getElementById("commonName").value
+  var sciName = document.getElementById("sciName").value
 
-    //const shortName = document.getElementById('shortName').value
-    //const sciName = document.getElementById('sciName').value
-//}
-//const form = document.getElementById('myForm')
-//form.addEventListener('sub-button', submitEvent)
-
-
-var shortName = []
-var plants = []
-
-
-
-
-    
-    
-    //document.getElementById("myForm").submit();
-    //console.log( "Requested Plants: " + shortName + sciName ) ;
-    //plants =+ (shortName, sciName)
-
- //
- function addPlant() { 
-    plants.push(document.getElementById("shortName").value);
-    plants.push(document.getElementById("sciName").value);
-    console.log(plants); //to confirm it has been added to the array 
- }
-
-  // Set the configuration for your app
-  // TODO: Replace with your project's config object
-  var config = {
-    apiKey: "AIzaSyBfUzdoYxk26tQj7MuQt9CU4Qj7TflOahA",
-    authDomain: "planter-8ffdc.firebaseapp.com",
-    databaseURL: "https://planter-8ffdc.firebaseio.com/",
-    storageBucket: "planter-8ffdc.appspot.com"
-  };
-  firebase.initializeApp(config);
-
-  // Get a reference to the database service
-  var database = firebase.database();
+  console.log(commonName + ' ' + sciName)
+  recommendations.push({
+    "commonName": commonName,
+    "sciName": sciName,
+});
+}
